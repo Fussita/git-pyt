69b43416b9690d1fdb48299c25d6a7fb330873c9
Fussita <hualong.chiang@gmail.com>
2025-05-12 19:54:40 -0400

    add

---
+++ api/main.py
@@ -0,0 +1,41 @@
+
+"""
+    python -m venv venv
+    venv\Scripts\activate
+    pip install "fastapi[standard]"
+    pip install pydantic
+    uvicorn main:app --reload
+"""
+
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel, EmailStr, Field
+
+class UserModel(BaseModel):
+    id: int = Field(..., ge=18, le=100) 
+    name: str = Field(min_length=6)
+    user: str = Field(min_length=6)
+    password: str = Field(min_length=6)
+    email: EmailStr = Field(min_length=6)
+ 
+app = FastAPI()
+users: list[UserModel] = []
+
+@app.get('/')
+def root():
+    return f'Bienvenido API'
+
+@app.get('/users', response_model=list[UserModel])
+def getUsers():
+    return users
+
+@app.post('/create')
+def createUser( user: UserModel ):
+    users.append(user)
+
+@app.get("/users/{id}", response_model=UserModel)
+# UNION[UserModel, str]
+def getUser(id: int):
+    for i in users:
+        if (i.id == id):
+            return i
+    raise HTTPException(status_code=404, detail="Usuario no encontrado")
